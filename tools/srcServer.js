130aba35a824d9ab9f901280933f360c8304db05
Chirag Doctor <doctorc@cpwplc.com>
2019-04-14 16:23:27 +0530

    stacked bar chart using d3

---
+++ tools/srcServer.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import webpack from 'webpack';
+import path from 'path';
+import config from '../webpack.config.dev';
+import open from 'open';
+
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+const sampleJson = require('../data/sample');
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join( __dirname, '../src/index.html'));
+});
+
+app.get('/data', function(req, res) {
+  res.json(sampleJson);
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+  }
+});
